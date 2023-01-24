1df0738abc6c6c62efa2b11581a21afe87012173
Palavuk <lit.nk.98@gmail.com>
2023-01-24 16:33:59 +0300

    success of first task, simple post filter

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+import pandas as pd
 
 app = FastAPI()
+
+@app.post("/filter/case_sensitive")
+def function_filter(data = Body()):
+    result = []
+    not_interested = []
+    for i in range(len(data) - 1):
+        word = data[i].lower()
+        if word in not_interested: continue
+        if data[i] not in (data[:i] + data[(i+1):]):
+            if word not in result: result.append(word)
+        elif word in result: 
+            result.remove(word)
+        else: 
+            not_interested.append(data[i].lower())
+    return result
